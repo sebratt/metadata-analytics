bb71a34fe2e7d179c0a57e0a9a22c6ebe9619b1e
Sarah Bratt <sebratt@syr.edu>
2023-03-07 13:46:16 -0700

    update with cases examples

---
+++ Disambig_JoysAuthorsCCv2 (1).R
@@ -0,0 +1,80 @@
+# All authors 
+# Can we use Jun's semantic scholar disambiguated authors approach to ascertain 
+# the authors are disambiguated? 
+
+# Steps would be:
+# 1. Find the GenBank IDs for all of Joy's / Jian's authors. 
+# 2. Search Jun's file 'genbank_semanticscholar_publication_author_with...csv'
+# 3. Merge subset of matches with "genbank_published_author_info_202108.csv'
+# 4. Identify names to confirm author id and name are reconciled.
+# 5. Add the author ID to the spreadsheet "authors for lookup" (n=400)
+library(readr)
+#install.packages("splitstackshape")
+#install.packages("dplyr")
+library(splitstackshape)
+library(readxl)
+library(dplyr)
+
+Author_tracking <- read_xlsx("Author_tracking_QLiu.xlsx") # read in author data - ambiguous
+
+ss_genbank_authors <- read_csv("genbank2021/csv_name_disambiguation/genbank_semanticscholar_publication_author_with_genbank_reference_ids.csv")
+# semantic scholar data from Jun ^^
+
+# Match Joy GenBank IDs to SS GenBank IDS 
+ids_Joy <- Author_tracking[,5]
+colnames(ids_Joy)[1] <- "Genbank_ids"
+colnames(ss_genbank_authors)[3] <-  "Genbank_ids"
+
+ids_Joy2 <- cSplit(ids_Joy, "Genbank_ids", sep = ",", direction = "long")
+
+result <- merge(x = ss_genbank_authors, y= ids_Joy2, by ="Genbank_ids", all.y = TRUE)
+
+#ids_Joy3 <- cSplit(ids_Joy2, "Genbank_ids", sep = "1A", direction = "wide") #didn't work because replaces comma, removing important info 
+
+#not <- table(is.na(result$ssid)) #2119 T = 1675
+#not3 <- table(is.na(result2$ssid)) #2119 T = 1675
+
+# Need to fix by getting R to programmatically  you know how to get R to programmatically "Find "1A" or "2A" or "3A" or "1B" or "2B" or "3B" patterns and insert a comma before it?
+
+author_name_jun <- read_csv("genbank2021/csv_name_disambiguation/genbank_published_author_info_202108.csv")
+#View(genbank_published_author_info_202108)
+
+result2 <- cSplit(result, "author_id_list", sep = " ", direction = "long")
+colnames(result2)[3] <-  "author_id"
+
+result3 <- merge(x = result2, y= author_name_jun, by ="author_id", all.x = TRUE)
+
+# Check ambiguous authors: is the problem solved with Jun's SS approach? 
+# example 1:  Fujiwara T. (a name in Joy's list that has >10 papers per year i.e. 17)
+
+fujiwara_ids <- c('AB025228', 'AF219994', 'AB015223', 'AB024763', 'AF219990', 'AB032920', 'AB045381, 
+                  AB045382', 'AJ299430', 'AJ299431', 'AF233369', 'AB049211', 'AJ278286', 'AB060300', 
+                  'AJ277440', 'AY043290', 'AB033823', 'AB064943', 'AY043484', 'AB073713')
+colnames(fujiwara_ids)[1] <- "match_ids_GB"
+
+result3 <- cSplit(result3, "Genbank_ids", sep='_1', direction = 'wide')
+colnames(result3)[10] <- "match_ids_GB"
+colnames(fujiwara_ids)[1] <- "match_ids_GB"
+m <- merge(x = result3, y = fujiwara_ids, by = "match_ids_GB")
+
+write.csv(fujiwara, "fujiwara_disambig.csv")
+
+
+# Check Result 3
+result4 <- result3[,c(1,7,2,4,5,6,8,9,10)]
+
+# example 2:  Cao,J.. (a name in Joy's list that has >5 papers per year)
+
+# example 3:  Tamura,T. (a name in Joy's list that has >5 papers per year)
+
+# example 4:  Wang,K.. (a name in Joy's list that has >5 papers per year)
+
+# example 5: Cui 
+
+
+result4$iz.na <- is.na(result4[,2])
+table(result4$iz.na)
+result5<-result4[!result4$iz.na,]
+write.csv(result5, "disambig_verify.csv")
+
+
